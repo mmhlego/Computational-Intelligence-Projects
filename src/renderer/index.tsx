b360f204c11c40873d3fce3543dd7a043a651dc4
mmhlego <mmhlego@gmail.com>
2022-11-08 15:56:21 +0330

    Added highlight.js

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { ContextProvider } from './MainContext';
+import '../../node_modules/highlight.js/styles/atom-one-dark.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
